abcd99c8c87bba745dc380c62b0455a7d841a26a
Matthew J. Lewis, Ph.D <lightscalar@gmail.com>
2017-09-05 06:50:41 -0400

    Changed API url.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 window.axios = axios
 
-const BASE_URL = 'http://localhost/api'
+const BASE_URL = 'http://45.55.145.118/api'
 // const BASE_URL = 'http://nvs-serve.ngrok.io'
 
 // Basic API for talking to the webserver API.
